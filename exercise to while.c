8590dcf4f9fe3fe60798875a102213bfbd57db1d
MandinuBala <135606566+MandinuBala@users.noreply.github.com>
2023-06-26 15:23:18 +0530

    loop exercises

---
+++ exercise to while.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    //do while loop
+
+    /*int x=5;
+    do
+    {
+        printf("%d ",x);
+        x=x+5;
+    }while (x<=100);
+
+    //for lop
+
+    /int x;
+
+    for(x=5;x<=100;x=x+5)
+    {
+        printf ("%d ",x);
+    }
+
+
+    //while
+
+    */ int x=5;
+    while (x<=100)
+    {
+        printf ("%d ",x);
+        x+=5; //x=x+5
+
+    }
+}
